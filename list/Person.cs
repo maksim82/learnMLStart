7c4678ace9e4314a5546d5ef54a4abbafc426fb8
maksim82 <71788767+maksim82@users.noreply.github.com>
2023-01-12 18:01:43 +0300

    Add files via upload

---
+++ list/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string toaString()
+        {
+            return "Имя пользователя - " + _name + " / Возраст - " + _age;
+        }
+    }
+}
---
+++ list/Program.cs
@@ -0,0 +1,17 @@
+﻿using list;
+using System.Collections;
+
+ArrayList list = new ArrayList();
+list.Add(new Person("maks", 12).toaString());
+list.Add(new Person("any", 17).toaString());
+list.Add(new Person("adrian", 16).toaString());
+list.Add(new Person("alex", 5).toaString());
+list.Add(new Person("joj", 10).toaString());
+list.Add(new Person("mark", 22).toaString());
+list.Add(new Person("mari", 19).toaString());
+list.Add(new Person("gabe", 43).toaString());
+
+foreach (object o in list)
+{
+    Console.WriteLine(o);
+}
